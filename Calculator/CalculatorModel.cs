be167476161cc19b0bfa87a274d653138c7611ac
AndreyShir <Andrey@shir.su>
2021-03-30 16:43:53 +0700

    Initial commit

--- Calculator/CalculatorModel.cs
+++ Calculator/CalculatorModel.cs
@@ -15,9 +15,51 @@ namespace Calculator
 
         public object Calculate(int operand1, int operand2, char operation)
         {
-            return null;
+            switch (operation)
+            {
+                case '+':
+                    return sumCalc(operand1,operand2);
+                    break;
+                case '-':
+                    return difCalc(operand1,operand2);
+                    break;
+                case '/':
+                    return divCalc(operand1,operand2);
+                    break;
+                case '*':
+                    return multCalc(operand1,operand2);
+                    break;
+                default:
+                    return null;
+                    break;
+            
+            }
         }
 
         
+    
+        private static int sumCalc(int operand1,int operand2)
+        {   
+                return operand1 = operand1 + operand2;
+        }
+        private static int difCalc(int operand1,int operand2)
+        {   
+                return operand1 = operand1 - operand2;
+        }
+        private static object divCalc(int operand1,int operand2)
+        {   
+                try
+                { 
+                    return operand1 = operand1 / operand2;
+                }
+                catch (DivideByZeroException) when (operand2 == 0)
+                {
+                    return null;
+                }
+        }   
+            private static int multCalc(int operand1,int operand2)
+        {   
+                return operand1 = operand1 * operand2;
+        }
     }
 }
